6fdba395af1a6556ca22a9d7ef99f67a110675dc
Jongjate Choomjairug <jongjate@isoftel.local>
2023-06-26 11:48:07 +0700

    feat:timmer

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ config();
 
 const app = express();
 const port = process.env.PORT || 6000
-const lineToken = process.env.LineNotifyToken 
+const lineToken = process.env.LineNotifyToken
 const totalTimers = parseInt(process.env.TOTAL_TIMERS) || 0;
 
 console.log(totalTimers)
@@ -21,31 +21,31 @@ app.listen(port, () => {
     console.log(`app running on port ${port}`)
 })
 
-app.get(`/api/`,(req,res)=>{
-    res.json({message:'connect api sucessfully'})
+app.get(`/api/`, (req, res) => {
+    res.json({ message: 'connect api sucessfully' })
 })
 
 function sendLineNotifyMessage(message) {
     const lineToken = process.env.LineNotifyToken;
-  
+
     request.post({
-      url: 'https://notify-api.line.me/api/notify',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${lineToken}`
-      },
-      form: {
-        message: message
-      }
+        url: 'https://notify-api.line.me/api/notify',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': `Bearer ${lineToken}`
+        },
+        form: {
+            message: message
+        }
     }, (err, httpResponse, body) => {
-      if (err) {
-        console.error('Error sending Line Notify message:', err);
-      } else {
-        console.log('Line Notify message sent:', body);
-      }
+        if (err) {
+            console.error('Error sending Line Notify message:', err);
+        } else {
+            console.log('Line Notify message sent:', body );
+        }
     });
-  }
-  
+}
+
 
 app.post(`/api/linesendnotify`, (req, res) => {
     const message = req.body.message;
@@ -78,19 +78,23 @@ app.post(`/api/linesendnotify`, (req, res) => {
 
 
 const schedules = Object.keys(process.env)
-  .filter(key => key.startsWith('SCHEDULE_'))
-  .map(key => process.env[key]);
+    .filter(key => key.startsWith('SCHEDULE_'))
+    .map(key => process.env[key]);
 
 // Determine the schedule count
 const scheduleCount = schedules.length;
 
+console.log(`Schedule count: ${scheduleCount}`)
+
 // Schedule the function for each schedule
 schedules.forEach((schedule) => {
-  cron.schedule(schedule, () => {
-    sendLineNotifyMessage(schedule);
-  }, {
-    timezone: 'Asia/Bangkok'
-  });
+    cron.schedule(schedule, () => {
+        const minute = schedule.slice(0,2);
+        const hour = schedule.slice(3,5)
+        sendLineNotifyMessage(`function running at ${hour} : ${minute}`);
+    }, {
+        timezone: 'Asia/Bangkok'
+    });
 });
 
 
